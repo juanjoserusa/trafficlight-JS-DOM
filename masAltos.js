d23e09ff125ec5159a6950ec007af119e1cd7e5a
unknown <juanjoserusa@gmail.com>
2023-12-22 18:49:02 +0100

    nuevo commit

---
+++ masAltos.js
@@ -0,0 +1,4 @@
+
+
+
+module.exports(zapatos)
---
+++ script2.js
@@ -0,0 +1,26 @@
+const zapatos = [{
+    "nombre": "Toscana negro",
+    "descripcion": "Zapatos con alzas. Exterior de piel flor de primera calidad. Interior totalmente forrado en fina piel. Suela de goma EVA ultraligera.",
+    "precio": 134.00,
+    "img" : "https://www.masaltos.com/3249-home_default/toscana-negro.jpg",
+    "enlace": "./producto sandalias.html"
+},{
+    "nombre": "Toscana negro",
+    "descripcion": "Zapatos con alzas. Exterior de piel flor de primera calidad. Interior totalmente forrado en fina piel. Suela de goma EVA ultraligera.",
+    "precio": 134.00,
+    "img" : "https://www.masaltos.com/3249-home_default/toscana-negro.jpg"
+},{
+    "nombre": "Toscana negro",
+    "descripcion": "Zapatos con alzas. Exterior de piel flor de primera calidad. Interior totalmente forrado en fina piel. Suela de goma EVA ultraligera.",
+    "precio": 134.00,
+    "img" : "https://www.masaltos.com/3249-home_default/toscana-negro.jpg"
+},{
+    "nombre": "Toscana negro",
+    "descripcion": "Zapatos con alzas. Exterior de piel flor de primera calidad. Interior totalmente forrado en fina piel. Suela de goma EVA ultraligera.",
+    "precio": 134.00,
+    "img" : "https://www.masaltos.com/3249-home_default/toscana-negro.jpg"
+}]
+
+
+
+
